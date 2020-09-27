07c82ce1449792ad69807a256c84ccbce73d06ba
Piotr <piotr.loboda@gmail.com>
2020-09-27 09:35:04 +0000

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "plobod-notes"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://9guxq3fhik.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_YPOnSn5Zl",
+    APP_CLIENT_ID: "7cbqle0uqokt01kcogfgikslul",
+    IDENTITY_POOL_ID: "us-east-1:e87c8320-3c72-4426-8969-2d163bfc4e3c"
+  }
+};
